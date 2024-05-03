084266e0d38f71b975b15fc04d1de14672afaed3
Augustin-joe <augustinjoe2003@gmail.com>
2024-05-03 10:47:29 +0530

    Initial commit

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,9 +1,23 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'dummy/config/environment';
+import AddonDocsRouter, {docsRoute} from 'ember-cli-addon-docs/router';
+import config from './config/environment';
 
-export default class Router extends EmberRouter {
+export default class Router extends AddonDocsRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  docsRoute(this, function () {
+    // this.route('compatibility');
+    // this.route('installation');
+    this.route('usage');
+    // this.route('dropdownevents');
+    // this.route('triggerevents');
+    // this.route('position');
+    // this.route('disabled');
+    // this.route('overlays');
+
+
+  });
+  this.route('not-found', { path: '/*path' });
+});
